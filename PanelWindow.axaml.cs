5a81ad4c14420a7a592731a5d00891253fd333d0
marcinekCoding <marcin.pawlak2@icloud.com>
2026-06-25 16:31:50 +0200

    clicker

--- PanelWindow.axaml.cs
+++ PanelWindow.axaml.cs
@@ -6,9 +6,16 @@ namespace MojeLogowanieGUI;
 
 public partial class PanelWindow : Window
 {
+    public static int punkty = 0;
+
     public PanelWindow(string login)
     {
         InitializeComponent();
         UserLogin.Text = "Twoj login: "+ login;
+    }
+    public void dodajPunkty(object sender, Avalonia.Interactivity.RoutedEventArgs e)
+    {
+        punkty++;
+        Points_box.Text = punkty.ToString();
     }
 }
